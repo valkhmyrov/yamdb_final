5a9bd820ea435fafc991e743e522db26da7417e9
Valentin Khmyrov <valkhmyrov@gmail.com>
2021-02-13 02:39:18 +0300

    worflow chaged

--- api/permissions.py
+++ api/permissions.py
@@ -5,8 +5,7 @@ class IsAuthorOrAdminOrReadOnly(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return (
-            request.method in permissions.SAFE_METHODS 
-            or (
+            request.method in permissions.SAFE_METHODS or (
                 request.user.is_authenticated and (
                     obj.author == request.user or request.user.is_admin or request.user.is_moderator
                 )
