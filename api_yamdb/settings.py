b4f96a822f6f2629a668f3b03a728db72f07a3c0
Valentin Khmyrov <valkhmyrov@gmail.com>
2021-02-14 22:49:30 +0300

    worflow chaged

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -2,7 +2,8 @@ import os
 from datetime import timedelta
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
+#SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = False
 ALLOWED_HOSTS = ['*']
 
